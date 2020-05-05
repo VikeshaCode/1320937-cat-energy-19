addc0e0a753457e1ff87f168de75b9462e34145e
VikeshaCode <Vikeshadoc@gmail.com>
2020-05-05 16:53:12 +0300

    Add interactive map

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+var header = document.querySelector(".header");
+var headerToogle = document.querySelector(".header__toogle");
+
+header.classList.remove("header--nojs");
+
+headerToogle.addEventListener("click", function () {
+  if (header.classList.contains("header--closed")) {
+    header.classList.remove("header--closed");
+    header.classList.add("header--opened");
+  } else {
+    header.classList.add("header--closed");
+    header.classList.remove("header--opened");
+  }
+});
+
